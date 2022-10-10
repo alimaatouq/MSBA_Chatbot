e796e8b0b821a9dbf67dc54e33fc57f625fb2f64
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-10-10 18:59:50 +0300

    Add files via upload

---
+++ trial.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
+
+trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" async></script>"""
+# Execute your app
+html(trial, width=980, height=600)
