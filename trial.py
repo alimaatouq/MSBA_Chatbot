988505d8733dfe06129e7ad34fa4aec386ee1aad
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-11-11 11:58:07 +0200

    Update trial.py

--- trial.py
+++ trial.py
@@ -3,6 +3,10 @@ from streamlit.components.v1 import html
 
 st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
 
+col1, col2, col3 = st.columns(3)
+
 trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" async></script>"""
-# Execute your app
-html(trial, height=600)
+
+with col2:
+  # Execute your app
+  html(trial, height=600)
