bc9727a608d0da45f8c02ede7c8181ded153fe1a
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-11-20 15:33:57 +0200

    Create trial_2.py

---
+++ trial_2.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns([1, 4, 1])
+
+trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" async></script>"""
+
+with col2:
+  # Execute your app
+  html(trial, height=600)
