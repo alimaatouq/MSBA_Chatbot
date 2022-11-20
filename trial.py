3225beb5903e30ab6cf1f0284d36f0df9e6e0985
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-11-20 13:23:22 +0200

    Update trial.py

--- trial.py
+++ trial.py
@@ -3,7 +3,7 @@ from streamlit.components.v1 import html
 
 st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([2, 4, 2])
+col1, col2, col3 = st.columns([1, 4, 1])
 
 trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" async></script>"""
 
