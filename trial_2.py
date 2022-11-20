8a19fc7db537fb3fcf98ea494569be3fd9e366e6
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-11-20 15:46:00 +0200

    Update trial_2.py

--- trial_2.py
+++ trial_2.py
@@ -10,3 +10,13 @@ trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" asy
 with col2:
   # Execute your app
   html(trial, height=600)
+with col2:
+  # Execute your app
+  html(trial, height=600)
+  
+with col1:
+  st.markdown("<h1 style='color: #1e54e4;'>Your MSBA Assistant is a click away!</h1>", unsafe_allow_html=True)
+  st.image("chat-bot-42.png")
+ 
+with col3:
+  st.markdown("<h1 style='color: #1e54e4;'>Made by MSBA Students for MSBA Students!</h1>", unsafe_allow_html=True)
