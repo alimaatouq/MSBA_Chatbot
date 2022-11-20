981b24146f4bab42ce52c93daa1f8fd291cfffdd
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2022-11-20 15:56:45 +0200

    Update trial_2.py

--- trial_2.py
+++ trial_2.py
@@ -4,6 +4,39 @@ st.set_page_config(page_title="MSBA Assistant", layout="wide", page_icon= "🤖"
 
 st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
 
+def get_base64_of_bin_file(bin_file):
+    """
+    function to read png file
+    ----------
+    bin_file: png -> the background image in local folder
+    """
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+    background: url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v960-ning-30.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=63dd5f402645ef52fb7dfb592aec765a");
+    background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+set_bg_hack_url()
+
+
 col1, col2, col3 = st.columns([1, 1.5, 1])
 trial = """<script src="//code.tidio.co/29ahjkkgzjmufusyxfai4rhrqquki8xg.js" async></script>"""
 
